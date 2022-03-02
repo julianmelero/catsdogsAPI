5d32a884cf764ad21700eb9dc9a1e0d793f965eb
Julián Melero <hola@julianmelero.com>
2022-03-02 12:07:38 +0100

    Change h5 and convert file

--- app/routers/catsdogs.py
+++ app/routers/catsdogs.py
@@ -1,6 +1,7 @@
 #Python
 
 import os
+import json
 
 
 # FastAPI
@@ -26,18 +27,36 @@ def post_image(
 ):
     if image.content_type not in IMAGES_ACCEPTED:
         raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,detail="Image accepted type " + str(IMAGES_ACCEPTED))
-
+        
     # I load the image with Pillow
-    imagen = Image.open(BytesIO(image.file.read()))
+    imagen = Image.open(BytesIO(image.file.read()))       
+    # imagen = np.array(imagen).astype(float) 
+    # image = cv2.resize(imagen,(100, 100)) # Thats becouse we need all images with the same size.
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Set images to white and black color.
+    # image = image.reshape (100,100,1) # 1 becouse only has 1 channel of color. 
+    # Image gray, because I need 1 channel
+    # img_gray = cv2.cvtColor(imagen, cv2.COLOR_RGB2GRAY)
     # Resize Image for requirements model
-    imagen_resize = imagen.resize((150,150))    
-    # Load to numpy as array
-    image_np = np.array(imagen_resize)#.astype('uint8') 
-    # image_np.reshape([0,255,255,3])
-    image_np[0] = image_np[0] / 255
-    image_np[1] = image_np[1] / 255   
-    image_np = np.expand_dims(image_np,0)
 
+
+    # imagen_resize = imagen.resize((100,100))
+    imagen = np.array(imagen).astype('uint8')
+    imagen[0] = imagen[0] / 255
+    imagen[1] = imagen[1] / 255  
+    imagen = cv2.resize(imagen,(100, 100))
+    imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    imagen = imagen.reshape(100,100,1)
+
+
+
+
+    # Load to numpy as array
+    # image_np = np.array(imagen_resize).astype(float) 
+    # image_np.reshape([0,255,255,3])
+    # image_np[0] = image_np[0] / 255
+    # image_np[1] = image_np[1] / 255   
+    image_np = np.expand_dims(imagen,axis=0)
+    print(image_np.shape)
     
     # image_np = tf.reshape(imagen, [150,150])
     
@@ -47,11 +66,9 @@ def post_image(
     # image_np = np.array(img_gray).astype('uint8') 
     # Seek image for read again
     image.file.seek(0)
-    model =  keras.models.load_model(os.path.join("model","catsdogs.h5"))
-    print(model.predict(image_np))
+    model =  keras.models.load_model(os.path.join("model","dogs-cats-cnn-ad.h5"))
+    prediction = model.predict(image_np)
+    pred = prediction[0][0]
     return {
-        "Filename": image.filename,
-        "Format": image.content_type,
-        "Size(kb)": round(len(image.file.read()) / 1024,2),
-        "np": image_np.shape
+        "predict": int(pred)
     }
